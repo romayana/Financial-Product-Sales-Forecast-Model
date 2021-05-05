615cdcb4f420d3087b15e86aa409e2d67c219629
Manuel <manuelgonzalezprados.com>
2021-05-05 20:54:38 +0200

    'updated'

--- Frontend/callornot.py
+++ Frontend/callornot.py
@@ -8,17 +8,39 @@ st.subheader("Por favor, rellena el Cuestionario")
 
  
 # Cargamos el modelo
-pickle_in = open('clfforest_gs.pkl', 'rb') 
-clasificador = pickle.load(pickle_in)
+pickle_in  = open('clfforest_gs.pkl', 'rb') 
+clasificador = pickle.load(pickle_in )
+
+
+# Definicion del Cuestionario
+edad = st.slider('Edad del Cliente', min_value=25, max_value=90)
+
+saldo_captacion = st.number_input('Saldo en Cuenta del Cliente')
+saldo_financiacion = st.number_input('Financiacion del Cliente')
+
+seg_valor = st.selectbox('Segmento Valor', ['ALTO','MEDIO','BAJO'])
+seg_recorrido = st.selectbox('Recorrido cliente',['ALTO ', 'MEDIO ', 'BAJO ','NO CALCULADO']) 
+marca_ccte = st.selectbox('Tipo de Gestor',['ASESOR EN OFICINA', 'ASESOR EN INTERNET', 'SIN ASESOR'])
+camino_digital = st.selectbox('Para que usa la APP internet',['COMPRADOR', 'CONSULTIVO', 'TRANSACCIONAL', 'POCO USO','SIN USO'])
+
+marca_bp = st.radio('Es cliente Banca Personal', ['SI','NO'])
+saldo_ffii = st.radio('Tiene Fondos de Inversion', ['SI','NO'])
+saldo_plp = st.radio('Tiene Plan de Pension', ['SI','NO'])
+lp_dom_ingresos = st.radio('Tiene domiciliados la nomina', ['SI','NO'])
+lp_rbos = st.radio('Tiene domiciliados los recibos', ['SI','NO'])
+lp_seg_vida = st.radio('Tiene seguro de vida', ['SI','NO'])
+lp_seg_auto = st.radio('Tiene seguro de coche', ['SI','NO'])
+lp_seg_acc = st.radio('Tiene seguro de accidentes', ['SI','NO'])
+lp_tjta_cto = st.radio('Tiene tarjeta de credito', ['SI','NO'])
+lp_tjt_rev = st.radio('Tiene revolving', ['SI','NO'])
+
 
 # Definimos la función que realizará la predicción a partir de los datos que introduzcamos en el cuestionario.  
 def prediccion (edad, marca_bp, seg_valor, seg_recorrido, marca_ccte, camino_digital,
               saldo_ffii, saldo_plp, lp_dom_ingresos, lp_rbos,
               lp_seg_vida ,lp_seg_auto ,lp_seg_acc, lp_tjta_cto, lp_tjt_rev,
               saldo_captacion, saldo_financiacion ):   
-    
-     
-    
+      
     #Nº1--------------------------------------
     edad == edad
     
@@ -50,9 +72,9 @@ def prediccion (edad, marca_bp, seg_valor, seg_recorrido, marca_ccte, camino_dig
     if marca_ccte ==   "AF":
         marca_ccte = 0
     elif marca_ccte == "CCTE":
-        marca_ccte = 2
+        marca_ccte = 1
     else:
-        marca_ccte = 1 
+        marca_ccte = 0 
     
     #Nª6--------------------------------------    
     if camino_digital ==   "SIN USO":
@@ -129,41 +151,19 @@ def prediccion (edad, marca_bp, seg_valor, seg_recorrido, marca_ccte, camino_dig
         
     # Predicciones 
     prediccion = clasificador.predict( 
-        [[edad, marca_bp, seg_valor, seg_recorrido, marca_ccte,
-         camino_digital,saldo_ffii,saldo_plp,lp_dom_ingresos,
-         lp_rbos,lp_seg_vida,lp_seg_auto,lp_seg_acc,lp_tjta_cto,
-         lp_tjt_rev,saldo_captacion,saldo_financiacion]])
-     
+        [[edad, marca_bp, seg_valor, seg_recorrido, marca_ccte, camino_digital,
+              saldo_ffii, saldo_plp, lp_dom_ingresos, lp_rbos,
+              lp_seg_vida ,lp_seg_auto ,lp_seg_acc, lp_tjta_cto, lp_tjt_rev,
+              saldo_captacion, saldo_financiacion]])
+    
+        
     if prediccion == 0:
         pred = 'NO LLAMES'
+        
     else:
         pred = 'LLAMA'
+        
     return pred
-
-
-# Definicion del Cuestionario
-edad = st.slider('Edad del Cliente', min_value=25, max_value=90)
-#edad = st.number_input('Edad del Cliente')
-
-saldo_captacion = st.number_input('Saldo en Cuenta del Cliente')
-saldo_financiacion = st.number_input('Financiacion del Cliente')
-
-seg_valor = st.selectbox('Segmento Valor', ['ALTO','MEDIO','BAJO'])
-seg_recorrido = st.selectbox('Recorrido cliente',['ALTO ', 'MEDIO ', 'BAJO ','NO CALCULADO']) 
-marca_ccte = st.selectbox('Tipo de Gestor',['ASESOR EN OFICINA', 'ASESOR EN INTERNET', 'SIN ASESOR'])
-camino_digital = st.selectbox('Para que usa la APP internet',['COMPRADOR', 'CONSULTIVO', 'TRANSACCIONAL', 'POCO USO','SIN USO'])
-
-marca_bp = st.radio('Es cliente Banca Personal', ['SI','NO'])
-saldo_ffii = st.radio('Tiene Fondos de Inversion', ['SI','NO'])
-saldo_plp = st.radio('Tiene Plan de Pension', ['SI','NO'])
-lp_dom_ingresos = st.radio('Tiene domiciliados la nomina', ['SI','NO'])
-lp_rbos = st.radio('Tiene domiciliados los recibos', ['SI','NO'])
-lp_seg_vida = st.radio('Tiene seguro de vida', ['SI','NO'])
-lp_seg_auto = st.radio('Tiene seguro de coche', ['SI','NO'])
-lp_seg_acc = st.radio('Tiene seguro de accidentes', ['SI','NO'])
-lp_tjta_cto = st.radio('Tiene tarjeta de credito', ['SI','NO'])
-lp_tjt_rev = st.radio('Tiene revolving', ['SI','NO'])
-
 
 # Prediccion Final.
 if st.button("Prediccion"): 
@@ -172,5 +172,4 @@ if st.button("Prediccion"):
                  lp_rbos,lp_seg_vida,lp_seg_auto,lp_seg_acc,lp_tjta_cto,
                  lp_tjt_rev,saldo_captacion,saldo_financiacion) 
         st.success('Resultado * {}'.format(result))
-        
         
